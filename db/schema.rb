0621de35ec57ed4d76962a16cbbf2c257e8395cf
Dani Lyn <dlyn1110@gmail.com>
2019-01-22 04:06:38 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190122035848) do
+ActiveRecord::Schema.define(version: 20190122035604) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-  end
-
-  create_table "characters", force: :cascade do |t|
-    t.string "name"
-    t.integer "show_id"
   end
 
   create_table "networks", force: :cascade do |t|
